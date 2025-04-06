810cc0f5b7de1757a8e3274439f66ebd48d781cf
miRx923 <tingling_reminder906@simplelogin.com>
2025-04-06 11:35:55 +0200

    hotfix

--- src/otestovanie.py
+++ src/otestovanie.py
@@ -1,32 +1,31 @@
 # otestovanie modelu na jednom obrazku (moze sa neskor pouzit pri softverovej implementacii)
 
 import torch
-from torchvision import models
+import torchvision
 from PIL import Image
-from torchvision.transforms import transforms
 import matplotlib.pyplot as plt
 
 from my_transform import transform_data
 
 
-model = models.resnet18()
+model = torchvision.models.resnet18()
 
-state_dict = torch.load('DataNoise/models/model_1_state_dict.pth') # , map_location=torch.device('cpu')
+state_dict = torch.load('DataNoise/models/model_1_state_dict.pth')
 model.load_state_dict(state_dict)
 
 model.eval()  # prepnutie do eval módu
 
 # Definícia transformácií pre vstupný obrázok
-preprocess = transform_data(gaus=False, mean=0.5, std=0.5, pois=False, lam=1, snp=False)
+preprocess = transform_data(gaus=False, pois=False, snp=False)
 
-#img = Image.open('data/oxford-iiit-pet/images/great_pyrenees_147.jpg')
-img = Image.open('dod1.jpg')
+img = Image.open('data/oxford-iiit-pet/images/great_pyrenees_147.jpg')
+#img = Image.open('dog1.jpg')
 
 # Aplikácia transformácií
 img_tensor = preprocess(img)
 
 # zobrazenie upraveneho obrazka
-conversion_to_image = transforms.ToPILImage()
+conversion_to_image = torchvision.transforms.ToPILImage()
 img_for_showcase = conversion_to_image(img_tensor)
 
 plt.subplot(1, 2, 1)
@@ -45,8 +44,18 @@ img_tensor = img_tensor.unsqueeze(0)
 with torch.no_grad():
     output = model(img_tensor)
 
+
 # vysledok
-class_names = ["Cat", "Dog"] 
+class_names = ["Cat", "Dog"]
+
+# Tieto triedy sa budu pouzivat ked sa urobi model na 37 tried namiesto 2 tried
+#class_names = ['Abyssinian', 'American Bulldog', 'American Pit Bull Terrier', 'Basset Hound', 'Beagle', 
+#            'Bengal', 'Birman', 'Bombay', 'Boxer', 'British Shorthair', 'Chihuahua', 'Egyptian Mau', 
+#            'English Cocker Spaniel', 'English Setter', 'German Shorthaired', 'Great Pyrenees', 'Havanese', 
+#            'Japanese Chin', 'Keeshond', 'Leonberger', 'Maine Coon', 'Miniature Pinscher', 'Newfoundland', 
+#            'Persian', 'Pomeranian', 'Pug', 'Ragdoll', 'Russian Blue', 'Saint Bernard', 'Samoyed', 'Scottish Terrier', 
+#            'Shiba Inu', 'Siamese', 'Sphynx', 'Staffordshire Bull Terrier', 'Wheaten Terrier', 'Yorkshire Terrier'] 
+
 predicted_class = torch.argmax(output, dim=1).item() # 0 alebo 1
 predicted_name = class_names[predicted_class]
 print(f"Predikovaná trieda: {predicted_name}\n")
